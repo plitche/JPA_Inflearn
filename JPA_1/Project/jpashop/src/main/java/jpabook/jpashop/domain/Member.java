13956d4c9e377d940040c5a160e301e991d41863
plitche <kysu728@naver.com>
2024-09-14 18:59:50 +0900

    회원 리포지토리 개발

--- JPA_1/Project/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPA_1/Project/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -19,7 +19,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String username;
+    private String name;
 
     @Embedded
     private jpabook.jpashop.domain.item.Address Address;
