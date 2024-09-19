7066e0dde4dee95c160472154745cc7efdc6c277
plitche <kysu728@naver.com>
2024-09-19 21:03:14 +0900

    상품 서비스 개발

--- JPA_1/Project/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ JPA_1/Project/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional(readOnly = true) // 읽기 전용
 @RequiredArgsConstructor
 public class MemberService {
 
