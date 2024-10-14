2fb35cd6311e630e5f5d67711de51d6851872a34
plitche <kysu728@naver.com>
2024-10-14 20:44:01 +0900

    엔티티를 DTO로 변환 - 페치  조인 최적화

--- JPA_1/Project/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ JPA_1/Project/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -104,6 +104,14 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    // fetch join
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+            "select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
 }
 
 
