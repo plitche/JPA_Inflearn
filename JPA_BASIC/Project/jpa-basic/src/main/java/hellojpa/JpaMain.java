ea322c7d0513028285e3f359253eae1a8cf55c9b
plitche <kysu728@naver.com>
2024-10-19 20:52:42 +0900

    엔티티 매핑

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,6 +16,7 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /*
             // 영속 상태
             Member member = em.find(Member.class, 150L);
             member.setName("John");
@@ -24,7 +25,7 @@ public class JpaMain {
             em.detach(member); // 특정 엔티티만 준영속 상태로 변환
             em.clear(); // entityManager안에 있는 영속성 컨텍스트를 전체 삭제
             em.close(); // 영속성 컨텍스트를 종료
-
+            */
             tx.commit(); // transaction을 commit하는 시점에 쿼리가 실행됨
         } catch (Exception e) {
             tx.rollback();
--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,35 +1,35 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+// @Table(uniqueConstraints = "")
 public class Member {
 
     @Id
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    // DB와 다른 컬럼명을 쓰고 싶을 때, null 제약 조건
+    @Column(name = "name", nullable = false)
+    private String username;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private Integer age;
 
-    public String getName() {
-        return name;
-    }
+    @Enumerated(EnumType.STRING) // Enum타입을 사용하고 싶을 때
+    private RoleType roleType;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob // varchar를 넘어서는 큰 문자열 타입
+    private String description;
+
+    @Transient // DB와 관계없는 memory에서만 사용하고 싶을 때
+    private int temp;
 
     public Member() {
     }
