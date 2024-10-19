802e72dffa509fde362f2a78053f14efd5fc460d
plitche <kysu728@naver.com>
2024-10-19 19:36:35 +0900

    플러시(flush)

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,55 +16,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 등록
-            /*
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA"); // 여기까지가 비영속 상태
-            em.persist(member); // 객체를 저장한 상태 -> 영속 상태
-            */
+            // 플러시 : 영속성 컨텍스트의 변경 내용을 데이터베이스에 반영
+            /**
+             * 플러시 발생
+             * - 변경 감지
+             * - 수정된 엔티티 쓰기 지연 SQL 저장소에 등록
+             * - 쓰기 지연 SQL 저장소의 쿼리를 데이터베이스에 전송
+             *
+             * 플러시 하는 방법
+             * - em.flush()
+             * - 트랜잭션 커밋
+             * - JPQL 쿼리 실행
+             */
 
-            // 수정
-            /*
-            Member findMember = em.find(Member.class, 1L); // JPA를 통해서 가져오면 관리하기 시작하면서 변경된 부분이 있는지 체크한다.
-            findMember.setName("Hello JPA");
-            */
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            // JPQL: Member 객체를 대상으로 조회(테이블x)
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(5)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
-
-            // 비영속
-            Member member = new Member();
-            member.setId(10L);
-            member.setName("Jack");
-
-            // 영속
-            Member findMember = em.find(Member.class, 10L);
-            Member findMember2 = em.find(Member.class, 10L); // 영속 엔티티의 동일성 보장
-
-            System.out.println("findMember = " + (findMember == findMember2)); // true
-
-            //==========
-            //영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(150L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-            // persist해도 쿼리가 실행되지 않고 commit 하는 시점에 쿼리들을 모아서 한번에 실행함
-
-
-            //===== 엔티티 수정 변경 감지 =====
-            Member member3 = em.find(Member.class, 150L);
-            member3.setName("ZZZZ"); // persist 하지 않아도 변경감지(Dirty checking)하여 update쿼리가 자동으로 실행됨
-
+            em.flush();
+            // flush는 영속성 컨텍스트를 비우지 않음
+            // 트랜잭션이라는 작업 단위가 중요 - 커밋 직전에만 동기화하면 됨
 
             tx.commit(); // transaction을 commit하는 시점에 쿼리가 실행됨
         } catch (Exception e) {
