5024d4ef082a76ea3d69e56860427a1867d1281e
plitche <kysu728@naver.com>
2024-10-19 19:43:03 +0900

    준영속 상태

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,25 +16,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 플러시 : 영속성 컨텍스트의 변경 내용을 데이터베이스에 반영
-            /**
-             * 플러시 발생
-             * - 변경 감지
-             * - 수정된 엔티티 쓰기 지연 SQL 저장소에 등록
-             * - 쓰기 지연 SQL 저장소의 쿼리를 데이터베이스에 전송
-             *
-             * 플러시 하는 방법
-             * - em.flush()
-             * - 트랜잭션 커밋
-             * - JPQL 쿼리 실행
-             */
+            // 영속 상태
+            Member member = em.find(Member.class, 150L);
+            member.setName("John");
 
-            Member member = new Member(200L, "member200");
-            em.persist(member);
-
-            em.flush();
-            // flush는 영속성 컨텍스트를 비우지 않음
-            // 트랜잭션이라는 작업 단위가 중요 - 커밋 직전에만 동기화하면 됨
+            // 준영속 상태
+            em.detach(member); // 특정 엔티티만 준영속 상태로 변환
+            em.clear(); // entityManager안에 있는 영속성 컨텍스트를 전체 삭제
+            em.close(); // 영속성 컨텍스트를 종료
 
             tx.commit(); // transaction을 commit하는 시점에 쿼리가 실행됨
         } catch (Exception e) {
