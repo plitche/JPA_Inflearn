08c9c9fba0e1f228f7bca9fda4847715fc83327a
plitche <kysu728@naver.com>
2024-10-20 19:07:53 +0900

    기본 키 매핑

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
@@ -4,33 +4,38 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+// @SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 // @Table(uniqueConstraints = "")
 public class Member {
 
     @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+     @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에 insert가 되어야만 id를 알 수 있음
     private Long id;
 
     // DB와 다른 컬럼명을 쓰고 싶을 때, null 제약 조건
     @Column(name = "name", nullable = false)
     private String username;
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) // Enum타입을 사용하고 싶을 때
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob // varchar를 넘어서는 큰 문자열 타입
-    private String description;
-
-    @Transient // DB와 관계없는 memory에서만 사용하고 싶을 때
-    private int temp;
-
     public Member() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
