d7d90eb8c1073da9ce4c15617a096a1145d637ab
plitche <kysu728@naver.com>
2024-10-20 19:35:19 +0900

    단방향 연관관계 매핑

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,11 +16,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속 상태
-            Member member = new Member();
-            member.setUsername("John");
+            // 팀 저장
+            Team team = new Team();
+            team.setName("TeamA");;
+            em.persist(team);
 
+            // 회원 저장
+            Member member = new Member();
+            member.setUsername("member1");
+//            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
+
+            // 조회
+            Member findMember = em.find(Member.class, member.getId());
+
+//            Long findTeamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
+
+            // 수정
+            Team newTeam = em.find(Team.class, 100L);
+            findMember.setTeam(newTeam);
 
             tx.commit();
         } catch (Exception e) {
--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
@@ -4,22 +4,29 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-// @SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
-// @Table(uniqueConstraints = "")
 public class Member {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
-     @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에 insert가 되어야만 id를 알 수 있음
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    // DB와 다른 컬럼명을 쓰고 싶을 때, null 제약 조건
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member() {
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
     public Long getId() {
