d07bcd14cdf58ae4c5d6bf3105e1683fb36f3cbf
plitche <kysu728@naver.com>
2024-10-23 22:16:21 +0900

    연관관계 다대다[N:N]

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,7 +1,9 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 
 @Entity
@@ -26,6 +28,11 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+//    @ManyToMany
+//    @JoinTable(name = "MEMBER_PRODUCT")
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Team getTeam() {
         return team;
