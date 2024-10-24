aa18c15807967930ca63655526f83a58dec2b8a3
plitche <kysu728@naver.com>
2024-10-24 22:17:06 +0900

    Mapped Superclass - 매핑 정보 상속

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -16,19 +17,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("Aaa");
-            movie.setActor("bbbb");
-            movie.setName("바람과 함께 사라지다.");
-            movie.setPrice(10000);
+            Member member = new Member();
+            member.setUsername("user");
+            member.setCreatedBy("Kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie);
-
-            em.flush();
-            em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/Member.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.concurrent.locks.Lock;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
