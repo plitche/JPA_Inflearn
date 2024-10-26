2c98f7cd934d23a9301c5e7f34444200759176b6
plitche <kysu728@naver.com>
2024-10-26 18:10:32 +0900

    기본값 타입

---
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,24 @@
+package hellojpa;
+
+public class ValueMain {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = a;
+
+        a = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        Integer c = 10;
+        Integer d = c;
+
+        c = 20;
+
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+
+}
