60c688af73ab112b8739d2369ca1363f1a473a7e
plitche <kysu728@naver.com>
2024-10-27 22:05:46 +0900

    값 타입의 비교

--- JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/ValueMain.java
+++ JPA_BASIC/Project/jpa-basic/src/main/java/hellojpa/ValueMain.java
@@ -19,6 +19,18 @@ public class ValueMain {
 
         System.out.println("c = " + c);
         System.out.println("d = " + d);
+
+        System.out.println("=============");
+        int p = 10;
+        int q = 10;
+
+        System.out.println("q == p = " + (q == p));
+
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+
+        System.out.println("(address1 == address2) = " + (address1 == address2));
+        System.out.println("address1.equals(address2) = " + address1.equals(address2));
     }
 
 }
