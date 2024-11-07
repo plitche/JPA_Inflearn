e41bf6e1096702d3b957ebcef0d704e0d31fbe56
plitche <kysu728@naver.com>
2024-11-07 22:52:06 +0900

    JPA NamedQuery

--- DATA_JPA/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ DATA_JPA/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -72,4 +72,15 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(15);
         assertThat(result.size()).isEqualTo(0);
     }
+
+    @Test
+    public void namedQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        assertThat(result.get(0)).isEqualTo(m1);
+    }
 }
