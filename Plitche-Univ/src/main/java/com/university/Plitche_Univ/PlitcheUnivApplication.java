69b9989512d6ce892850016fafc31dbcc085428b
plitche <kysu728@naver.com>
2024-11-09 16:56:36 +0900

    Plitche University project init
    JPA Practice Project

---
+++ Plitche-Univ/src/main/java/com/university/Plitche_Univ/PlitcheUnivApplication.java
@@ -0,0 +1,13 @@
+package com.university.Plitche_Univ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlitcheUnivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlitcheUnivApplication.class, args);
+	}
+
+}
