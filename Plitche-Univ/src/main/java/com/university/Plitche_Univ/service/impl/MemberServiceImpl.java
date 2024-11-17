6e64eb687355cf47e80f30e5b2e53da2cd7f710c
plitche <kysu728@naver.com>
2024-11-17 17:52:00 +0900

    Edit Test Code

--- Plitche-Univ/src/main/java/com/university/Plitche_Univ/service/impl/MemberServiceImpl.java
+++ Plitche-Univ/src/main/java/com/university/Plitche_Univ/service/impl/MemberServiceImpl.java
@@ -3,20 +3,20 @@ package com.university.Plitche_Univ.service.impl;
 import com.university.Plitche_Univ.dto.request.MemberRequestDto;
 import com.university.Plitche_Univ.dto.response.MemberResponseDto;
 import com.university.Plitche_Univ.entity.Member;
-import com.university.Plitche_Univ.entity.embed.Address;
 import com.university.Plitche_Univ.repository.MemberRepo;
 import com.university.Plitche_Univ.service.MemberService;
 import com.university.Plitche_Univ.utiles.converter.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepo memberRepo;
@@ -24,6 +24,7 @@ public class MemberServiceImpl implements MemberService {
     private final Mapper mapper;
 
     @Override
+    @Transactional(readOnly = true)
     public List<MemberResponseDto> getMemberList(PageRequest pageRequest) {
         List<Member> findMembers = memberRepo.findAll(pageRequest);
         return findMembers.stream().map(
@@ -32,6 +33,7 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public MemberResponseDto getMemberById(String userId) {
         Member findMember = memberRepo.findMemberByLoginId(userId);
         return mapper.memberToResponseDto(findMember);
--- Plitche-Univ/src/test/java/com/university/Plitche_Univ/service/impl/MemberServiceImplTest.java
+++ Plitche-Univ/src/test/java/com/university/Plitche_Univ/service/impl/MemberServiceImplTest.java
@@ -2,27 +2,59 @@ package com.university.Plitche_Univ.service.impl;
 
 import com.university.Plitche_Univ.dto.request.MemberRequestDto;
 import com.university.Plitche_Univ.dto.response.MemberResponseDto;
-import com.university.Plitche_Univ.entity.Member;
 import com.university.Plitche_Univ.entity.embed.Address;
 import com.university.Plitche_Univ.enums.Grade;
 import com.university.Plitche_Univ.service.MemberService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class MemberServiceImplTest {
 
-    @Autowired
-    MemberService memberService;
+    @Autowired MemberService memberService;
+
+    @BeforeEach
+    void initData() {
+        for (int i = 0; i < 10; i++) {
+            Address address = new Address("Korea_" + i, "Seoul_" + i, i + "23-4567");
+            MemberRequestDto memberRequestDto = new MemberRequestDto();
+            memberRequestDto.setLoginId("plitche_" + i);
+            memberRequestDto.setPassword("1234");
+            memberRequestDto.setName("Yongsoo Kwon");
+
+            switch (i) {
+                case 0:
+                case 1:
+                case 2: memberRequestDto.setGrade(Grade.FRESHMAN); break;
+                case 3:
+                case 4:
+                case 5: memberRequestDto.setGrade(Grade.SOPHOMORE); break;
+                case 6:
+                case 7:
+                case 8: memberRequestDto.setGrade(Grade.JUNIOR); break;
+                default: memberRequestDto.setGrade(Grade.SENIOR); break;
+            }
+
+            memberRequestDto.setAddress(address);
+        }
+    }
+
+    @AfterEach
+    void clearData() {
+
+    }
 
     @Test
     public void memberSaveTest() {
         //given
-        Address address = new Address("Korean", "Seoul", "123-4567");
+        Address address = new Address("Korea", "Seoul", "123-4567");
         MemberRequestDto memberRequestDto = new MemberRequestDto();
         memberRequestDto.setLoginId("plitche");
         memberRequestDto.setPassword("1234");
