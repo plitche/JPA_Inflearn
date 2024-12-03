4fa2fabd6aa0ce6e5ff3084e374b25ce64232d25
plitche <kysu728@naver.com>
2024-12-03 21:29:27 +0900

    조인 - 기본 조인

--- Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -277,4 +277,43 @@ public class QuerydslBasicTest {
         Assertions.assertThat(teamB.get(team.name)).isEqualTo("teamB");
         Assertions.assertThat(teamB.get(member.age.avg())).isEqualTo(30);
     }
+
+    // 팀 A에 소속된 모든 회원
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+//                .innerJoin(member.team, team)
+//                .leftJoin(member.team, team)
+//                .rightJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        Assertions.assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    // 세타 조인
+    // 회원의 이름이 팀 이름과 같은 회원 조회
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        Assertions.assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+
+
+
 }
